b492cd80a8bc1a03e4ad5d43d0a54c8e5f37c84f
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-06-19 22:14:11 -0400

    Configurando la entidad Ordenes.

--- Models/Ordenes.cs
+++ Models/Ordenes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace RegistroPedidos.Models
 {
     public class Ordenes
     {
+        [Key]
+        public int OrdenId { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Debe selecionar un suplidor para realizar la ordén.")]
+        public int SuplidorId { get; set; }
+
+        public float Monto { get; set; }
+
+
+        public virtual List<OrdenesDetalle> Detalle { get; set; }
     }
 }
