66c43dc2a4e6c0be163391b8b3f07ae175295fff
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-06-19 22:15:55 -0400

    Configurando la entidad OrdenesDetalle.

--- Models/OrdenesDetalle.cs
+++ Models/OrdenesDetalle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace RegistroPedidos.Models
 {
     public class OrdenesDetalle
     {
+        [Key]
+        public int Id { get; set; }
+
+        public int OrdenId { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public float Costo { get; set; }
     }
 }
