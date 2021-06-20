ed1d6eeae9b6095f791996f6b005a75866fd5fc2
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-06-19 22:07:31 -0400

    Terminando de configura la entidad Productos.

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace RegistroPedidos.Models
 {
     public class Productos
     {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "Debe agregar una descripción del producto.")]
+        public string Descripcion { get; set; }
+       
+        public float Costo { get; set; }
+        
+        public float Inventario { get; set; }
     }
 }
