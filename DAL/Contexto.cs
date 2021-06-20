d8d78c982ea4607294bc287c7bf06848a9a96946
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-06-19 22:33:45 -0400

    Terminando de Configurar el contexto.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -20,7 +20,92 @@ namespace RegistroPedidos.DAL
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Suplidores>();
+            //Suplidores---------------------------------------------------------------------
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores {
+                SuplidorId = 1,
+                Nombres = "Anneury Andrés Sosa Abreu"
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 2,
+                Nombres = "Monserrat Blasco"
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 3,
+                Nombres = "Guillem Moron"
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 4,
+                Nombres = "Carme Aznar"
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 5,
+                Nombres = "Andrea Costas"
+            });
+
+            //Productos---------------------------------------------------------------------------------
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 1,
+                Descripcion = "Pepsi",
+                Costo = 20,
+                Inventario = 100
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 2,
+                Descripcion = "Kola Real",
+                Costo = 15,
+                Inventario = 100
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 3,
+                Descripcion = "Pastelito",
+                Costo = 20,
+                Inventario = 100
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 4,
+                Descripcion = "Chokies",
+                Costo = 25,
+                Inventario = 100
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 5,
+                Descripcion = "Jugo Petit",
+                Costo = 30,
+                Inventario = 100
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 6,
+                Descripcion = "Café",
+                Costo = 25,
+                Inventario = 100
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 7,
+                Descripcion = "Pan",
+                Costo = 10,
+                Inventario = 100
+            });
         }
     }
 }
